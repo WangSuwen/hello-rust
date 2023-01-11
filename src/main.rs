b0db91f71caaa35381a4e198a04865d5b10556c9
wangsuwen <1002853070@qq.com>
2023-01-11 08:17:33 +0800

    学习：关联类型、特征约束

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,87 @@
-// 方法
+// 关联类型 + 调用同名方法 + 特征约束
+/* use std::ops::Add;
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32
+}
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y
+        }
+    }
+}
 fn main () {
-    struct Rectangle {
-        width: u32,
-        height: u32 
-    }
-    impl Rectangle {
-        pub fn new(width: u32, height: u32) -> Self {
-            Rectangle { width, height }
-        }
-        fn area(&self) -> u32 {
-            self.width * self.height
-        }
-        pub fn width(&self) -> u32 {
-            self.width
-        }
-    }
+    assert_eq!(Point { x: 1, y: 2 } + Point { x: 2, y: 2}, Point { x: 3, y: 3 });
+} */
 
-    // let rect1 = Rectangle { width: 12, height: 25 };
-    // println!("Rectangle`s area is {}", rect1.area());
-    
-    let rect2 = Rectangle::new(32, 16);
-    // println!("rectangle`s width is {}", rect2.width());  // 结构体的实例 调用内部方法
-    println!("rectangle`s width is {}", Rectangle::width(&rect2));  // 结构体直接调用内部方法
+// 调用同名方法
+/* 
+trait Polit {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+struct Human;
+
+impl Polit for Human {
+    fn fly(&self) {
+        println!("Polit flying");
+    }
+}
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Wizard flying")
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("Human flying");
+    }
+}
+
+fn main () {
+    let person = Human;
+    person.fly(); // Human flying  调用 Human 类型自身的 fly 方法
+    Polit::fly(&person);  // Polit flying  调用特征Polit上的 fly 方法
+    Wizard::fly(&person); // Wizard flying
+} */
+
+// 特征约束
+use std::fmt;
+trait OutlinePrint:fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl OutlinePrint for Point {}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+fn main () {
+    let point = Point {
+        x: 12,
+        y: 12
+    };
+    point.outline_print();
 }
