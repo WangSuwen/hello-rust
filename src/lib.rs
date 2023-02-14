341f0be17309f067588eef144f9b7b950778152e
wangsuwen <1002853070@qq.com>
2023-02-14 17:01:19 +0800

    modules + doc 学习

--- src/lib.rs
+++ src/lib.rs
@@ -1,146 +1,43 @@
-// 模块功能
 
-/* mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {
-            println!("add to waitlist !!");
-        }
-    }
-}
-pub fn eat_at_restaurant() {
-    // 绝对路径
-    crate::front_of_house::hosting::add_to_waitlist();
-    // 相对路径
-    front_of_house::hosting::add_to_waitlist();
-} */
+#![allow(unused)]
+//! # Art
+//!
+//!  未来的艺术建模库，现在的调色库
 
-// 1、通过 rs 文件 引用其他的 lib
-/* mod front_of_house;
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
 
-pub use crate::front_of_house::hosting;
+pub mod kinds {
+    //! 定义颜色的类型
 
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
-} */
-
-// 2、通过 目录中的 rs 文件引用其他 lib
-
-/* mod front_of_house;
-pub use crate::front_of_house::hosting::{
-    add_to_waitlist,
-    add_to_waitlist2
-};
-// 1、使用 `as` 来避免名称重复
-// 2、去掉pub，将引入的函数，在此文件中变为私有属性，对外不可见
-use crate::front_of_house::hosting::add_to_waitlist3 as atw3;
-
-pub fn eat_at_restaurant() {
-    add_to_waitlist();
-    add_to_waitlist2();
-    atw3();
-} */
-
-// 综合示例： 一个名为 `my_mod` 的模块
-mod my_mod {
-    // 模块中的项默认具有私有的可见性
-    fn private_function() {
-        println!("called `my_mod::private_function()`");
+    /// 主色
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
     }
 
-    // 使用 `pub` 修饰语来改变默认可见性。
-    pub fn function() {
-        println!("called `my_mod::function()`");
-    }
-
-    // 在同一模块中，项可以访问其它项，即使它是私有的。
-    pub fn indirect_access() {
-        print!("called `my_mod::indirect_access()`, that\n> ");
-        private_function();
-    }
-
-    // 模块也可以嵌套
-    pub mod nested {
-        pub fn function() {
-            println!("called `my_mod::nested::function()`");
-        }
-
-        #[allow(dead_code)]
-        fn private_function() {
-            println!("called `my_mod::nested::private_function()`");
-        }
-
-        // 使用 `pub(in path)` 语法定义的函数只在给定的路径中可见。
-        // `path` 必须是父模块（parent module）或祖先模块（ancestor module）
-        pub(in crate::my_mod) fn public_function_in_my_mod() {
-            print!("called `my_mod::nested::public_function_in_my_mod()`, that\n > ");
-            public_function_in_nested()
-        }
-
-        // 使用 `pub(self)` 语法定义的函数则只在当前模块中可见。
-        pub(self) fn public_function_in_nested() {
-            println!("called `my_mod::nested::public_function_in_nested");
-        }
-
-        // 使用 `pub(super)` 语法定义的函数只在父模块中可见。
-        pub(super) fn public_function_in_super_mod() {
-            println!("called my_mod::nested::public_function_in_super_mod");
-        }
-    }
-
-    pub fn call_public_function_in_my_mod() {
-        print!("called `my_mod::call_public_funcion_in_my_mod()`, that\n> ");
-        nested::public_function_in_my_mod();
-        print!("> ");
-        nested::public_function_in_super_mod();
-    }
-
-    // `pub(crate)` 使得函数只在当前包中可见
-    pub(crate) fn public_function_in_crate() {
-        println!("called `my_mod::public_function_in_crate()");
-    }
-
-    // 嵌套模块的可见性遵循相同的规则
-    mod private_nested {
-        #[allow(dead_code)]
-        pub fn function() {
-            println!("called `my_mod::private_nested::function()`");
-        }
+    /// 副色
+    #[derive(Debug,PartialEq)]
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
     }
 }
 
-fn function() {
-    println!("called `function()`");
-}
+pub mod utils {
+    //! 实用工具，目前只实现了调色板
+    use crate::kinds::*;
 
-fn main() {
-    // 模块机制消除了相同名字的项之间的歧义。
-    function();
-    my_mod::function();
-
-    // 公有项，包括嵌套模块内的，都可以在父模块外部访问。
-    my_mod::indirect_access();
-    my_mod::nested::function();
-    my_mod::call_public_function_in_my_mod();
-
-    // pub(crate) 项可以在同一个 crate 中的任何地方访问
-    my_mod::public_function_in_crate();
-
-    // pub(in path) 项只能在指定的模块中访问
-    // 报错！函数 `public_function_in_my_mod` 是私有的
-    //my_mod::nested::public_function_in_my_mod();
-    // 试一试 ^ 取消该行的注释
-
-    // 模块的私有项不能直接访问，即便它是嵌套在公有模块内部的
-
-    // 报错！`private_function` 是私有的
-    //my_mod::private_function();
-    // 试一试 ^ 取消此行注释
-
-    // 报错！`private_function` 是私有的
-    //my_mod::nested::private_function();
-    // 试一试 ^ 取消此行的注释
-
-    // 报错！ `private_nested` 是私有的
-    //my_mod::private_nested::function();
-    // 试一试 ^ 取消此行的注释
+    /// 将两种主色调成副色
+    /// ```rust
+    /// use art::utils::mix;
+    /// use art::kinds::{PrimaryColor,SecondaryColor};
+    /// assert!(matches!(mix(PrimaryColor::Yellow, PrimaryColor::Blue), SecondaryColor::Green));
+    /// ```
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        SecondaryColor::Green
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,22 @@
-// 动态数组 Vector
+// 注释 + 文档
 
-fn main () {
+// 引入自定义的依赖库，以 【包根】为始 `hello_rust`
+use hello_rust::kinds::PrimaryColor; 
 
-    // 1、不指定类型时，第一次push后，会自动确定数组的类型
-    let mut v = Vec::new();
-    v.push(1);
-
-    // 2、显示的声明数组内元素的类型
-    let mut v1: Vec<String> = Vec::new();
-    v1.push(String::from("张三"));
-
-    // 3、创建指定长度的数组
-    let mut v2: Vec<i32> = Vec::with_capacity(6);
-    v2.push(1);
-    v2.push(2);
-    v2.push(3);
-    v2.push(4);
-    v2.push(5);
-    v2.push(6);
-    v2.push(7);
-    println!("v2 的长度： {}", v2.len());
-    
-    // 4、 使用宏 vec!
-    let v3 = vec![1, 2, 3, 4];
-    println!("v3 的长度： {}", v3.len());
-
-    // 5、从Vector 中读取元素 TODO: 注意： v[index] 方式会出现下标越界问题，v.get(index)则不会，内部处理的会返回None；
-    let v4 = vec![1, 2, 3, 4, 5, 6];
-    let third = &v4[2];
-    println!("v4 的第三个元素是： {}", third);
-    match v4.get(1) {
-        Some(match_val) if match_val < &3 => {
-            println!("match 匹配到值 {} 小于3", match_val);
-        },
-        Some(match_val @ 3..=5) => {
-            println!("match 在 [3:5]的范围内: {}", match_val);
-        },
-        Some(match_val) => {
-            println!("match 匹配到值了： {}", match_val);
-        },
-        None => println!("match 了个寂寞!")
+fn main() {
+    /// 文档注释
+    /// add_one 是一个给参数加1的函数
+    /// 
+    /// # Example
+    /// ```
+    /// let age = add_one(17);
+    /// println!("age is {}", age);
+    /// ```
+    /// 
+    /// 
+    pub fn add_one(x: i32) -> i32 {
+        x + 1
     }
-
+    let s = String::from("asdf");
+    println!("{}", s);
 }
