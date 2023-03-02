f8f0ea8a515d27a14e05530cfb7debd2930a105b
wangsuwen <1002853070@qq.com>
2023-03-02 10:10:53 +0800

    学习：生命周期 + Box

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,61 @@
-// 生命周期
+// Box 学习
+// 在 Rust 中，所有值默认都是栈分配的。通过创建 Box<T>，可以把值装箱（boxed）来使它在堆上分配。箱子（box，即 Box<T> 类型的实例）是一个TODO: 【智能指针】，指向堆分配的 T 类型的值
+
+use std::mem;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+#[allow(dead_code)]
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+fn origin() -> Point {
+    Point { x: 0.0, y: 0.0 }
+}
+
+fn boxed_origin() -> Box<Point> {
+    // 在堆上分配这个点（point），并返回一个指向它的指针
+    Box::new(Point { x: 0.0, y: 0.0 })
+}
+
 fn main() {
-    // 1、 悬垂指针， 下面代码中，x 在 } 之后就已经被释放了，然后 println! 中的r引用x时，引用了个寂寞，这就是所谓的【悬垂指针】
-    /* let r;
-    {
-        let x = 5;
-        r = &x;
-    }
-    println!("r: {}", r); */
-
-    // 2、生命周期的应用
-    /* let string1 = String::from("abcd");
-    let string2 = "xyz";
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
-
-    // 如果不加生命周期的话，会报错
-    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-        if x.len() > y.len() {
-            x
-        } else {
-            y
-        }
-    } */
-
-    /* fn longest<'a>(x: &str, y: &str) -> String {
-        let result = String::from("Hello, world!");
-        // result.as_str()  // 这里函数要求返回一个生命周期为'a的引用【 &'a str 】，但是声明周期在 } 处已结束，会导致悬垂引用，所以编译不过
-        result // 我们可以返回 String 的所有权，这样就不会有生命周期的问题了，但是需要将函数返回值改成 String
-    }
-    println!("{}", longest("", "")); */
-
-    /* #[derive(Debug)]
-    struct ImportantExcept<'a> {
-        part: &'a str
-    }
-
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    // println!("{}", first_sentence);
-    let i = ImportantExcept{
-        part: first_sentence
+    // （所有的类型标注都不是必需的）
+    // 栈分配的变量
+    let mut point: Point = origin();
+    point.x = 1.0;
+    let rectangle: Rectangle = Rectangle {
+        p1: origin(),
+        p2: Point { x: 3.0, y: 4.0 }
     };
-    println!("{:?}", i.part); */
 
-    use std::fmt::Display;
+    // 堆分配的 rectangle（矩形）
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        p1: origin(),
+        p2: origin()
+    });
 
-    fn longest_with_an_announcement<'a, T>(
-        x: &'a str,
-        y: &'a str,
-        ann: T,
-    ) -> &'a str
-    where
-        T: Display,
-    {
-        println!("Announcement! {}", ann);
-        if x.len() > y.len() {
-            x
-        } else {
-            y
-        }
-    }
+    // 函数的输出可以装箱
+    let mut boxed_point: Box<Point> = Box::new(origin());
+    boxed_point.x += 1.0;
+    // 两层装箱
+    let box_in_a_box: Box<Box<Point>> = Box::new(boxed_origin());
 
+    println!("Point occupies {} bytes in the stack", mem::size_of_val(&point));
+    println!("Rectangle occupies {} bytes in the stack", mem::size_of_val(&rectangle));
+    // box 的宽度就是指针宽度
+    println!("Boxed point occupies {} bytes in the stack", mem::size_of_val(&boxed_point));
+    println!("Boxed rectangle occupies {} bytes in the stack", mem::size_of_val(&boxed_rectangle));
+    println!("Boxed box occupies {} bytes in the stack", mem::size_of_val(&box_in_a_box));
+    // 将包含在 `boxed_point` 中的数据复制到 `unboxed_point`
+    let mut unboxed_point: Point = *boxed_point;
+    unboxed_point.x += 1.0;
+    println!("Unboxed point occupies {} bytes in the stack", mem::size_of_val(&unboxed_point));
+    println!("unboxed_point is {:?}, boxed_point is {:?}, point is {:?}", unboxed_point, boxed_point, point);
 }
