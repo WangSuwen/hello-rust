6cbd8eca63a7ace99ca66ab898f98d805ee16c55
wangsuwen <1002853070@qq.com>
2023-07-20 13:42:16 +0800

    学习：迭代器

---
+++ src/main-迭代器.rs
@@ -0,0 +1,116 @@
+// 迭代器 Iterator
+
+use std::collections::HashMap;
+
+
+fn main () {
+    /* let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("{}", val);
+    } */
+/* 
+    // Rust 中, for 循环只是 Iterator 的语法糖
+    // 将数组转换成 iterator 后，可以直接调用 next方法获取数组元素
+    let arr = [1, 2, 3];
+    let mut arr_iter = arr.into_iter();
+    assert_eq!(arr_iter.next(), Some(1));
+    assert_eq!(arr_iter.next(), Some(2));
+    assert_eq!(arr_iter.next(), Some(4));
+    assert_eq!(arr_iter.next(), None);
+     */
+
+    // 模拟实现 for 循环
+    /* let values = vec![1, 2, 3];
+    let result = match IntoIterator::into_iter(values) {
+        mut iter => loop {
+            match iter.next() {
+                Some(x) => {
+                    println!("{}", x);
+                },
+                None => break,
+            }
+        }
+    };
+    println!("{:?}", result); */
+    
+    /* 
+        into_iter, iter, iter_mut
+        在之前的代码中，我们统一使用了 into_iter 的方式将数组转化为迭代器，除此之外，还有 iter 和 iter_mut，聪明的读者应该大概能猜到这三者的区别：
+
+        into_iter 会夺走所有权
+        iter 是借用
+        iter_mut 是可变借用
+        其实如果以后见多识广了，你会发现这种问题一眼就能看穿，into_ 之类的，都是拿走所有权，_mut 之类的都是可变借用，剩下的就是不可变借用。
+        详见下方示例：
+     */
+    // 1、 into_inter 所有权发生转移
+    /* let values = vec![1, 2, 3];
+    for v in values.into_iter() {
+        println!("{}", v);
+    }
+    // println!("{:?}", values); // 这里会报错，因为 values 在 .into_inter() 是发生了所有权转移
+    
+    // 2、 iter 不可变借用
+    let values2 = vec![1, 2, 3];
+    let values2_iter = values2.iter();
+    // if let Some(v) = values2_iter.next() { println!("{}", v); }  // 这里会报错，因为 values2_inter 是不可变借用，调用 next 方法时，会变更其内部属性值，所以会报错
+    println!("{:?}", values2);
+    
+    // 3、 iner_mut 可变借用
+    let mut values3 = vec![1, 2, 3];
+    let mut values3_iter_mut = values3.iter_mut();
+    if let Some(x) = values3_iter_mut.next() {
+        println!("{}", x); // 这里的x 是个可变引用
+        *x = 0; // 这里通过  * 解引用，取得 x 指向的值，并修改它的值
+    }
+    println!("{:?}", values3); */
+    // 4、collect 将 适配器迭代器处理过的 迭代器进行收集，并返回一个新的迭代器
+    /* let names = ["zhangsan", "lisi"];
+    let ages = [16, 18];
+    let map: HashMap<_, _> = names.into_iter().zip(ages.into_iter()).collect();
+    println!("{:?}", map); */
+    // 5、实现自定义  Iterator
+    #[derive(Debug)]
+    struct Counter {
+        count: u32
+    }
+
+    impl Counter {
+        fn new() -> Counter {
+            Counter { count: 0 }
+        }
+    }
+
+    impl Iterator for Counter {
+        type Item = u32;
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.count < 5 {
+                self.count += 1;
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+    /* let mut counter = Counter::new();
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next());
+    println!("{:?}", counter.next()); */
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| {
+            println!("a: {:?}, b: {:?}", a, b);
+            a * b
+        })
+        .filter(|x| {
+            println!("x is: {:?}", x);
+            x % 3 == 0
+        })
+        .sum();
+    println!("{:?}", sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,42 @@
-// 迭代器 Iterator
-
+// 类型
 
 fn main () {
-    /* let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-
-    for val in v1_iter {
-        println!("{}", val);
+    /* let a: i32 = 10;
+    let b: u16 = 100;
+  
+    if a < b { // 或报错，并提示 将 u16 转换成 i32 类型
+      println!("Ten is less than one hundred.");
     } */
-/* 
-    // Rust 中, for 循环只是 Iterator 的语法糖
-    // 将数组转换成 iterator 后，可以直接调用 next方法获取数组元素
-    let arr = [1, 2, 3];
-    let mut arr_iter = arr.into_iter();
-    assert_eq!(arr_iter.next(), Some(1));
-    assert_eq!(arr_iter.next(), Some(2));
-    assert_eq!(arr_iter.next(), Some(4));
-    assert_eq!(arr_iter.next(), None);
-     */
+    /* let a = 3.1 as i8;
+    let b = 100_i8 as i32;
+    let c = 'a' as u8; // 将字符'a'转换为整数，97
 
-    // 模拟实现 for 循环
-    /* let values = vec![1, 2, 3];
-    let result = match IntoIterator::into_iter(values) {
-        mut iter => loop {
-            match iter.next() {
-                Some(x) => {
-                    println!("{}", x);
-                },
-                None => break,
-            }
+    println!("{},{},{}",a,b,c) */
+
+    // 内存地址转换成指针
+    /* #![allow(unused)]
+    let mut values: [i32; 2] = [1, 2];
+    let p1: *mut i32 = values.as_mut_ptr();
+    let first_address = p1 as usize; // 将p1内存地址转换为一个整数
+    let second_address = first_address + 4; // 4 == std::mem::size_of::<i32>()，i32类型占用4个字节，因此将内存地址 + 4，即：指针向后移一位
+    let p2 = second_address as *mut i32; // 将移动后的新地址转成指针
+    unsafe {
+        println!("p1: {:?}, first_address: {}, second_address: {},  p2: {:?}, 指针p2指向的值: {:?}", p1, first_address, second_address, p2, *p2);
+    }
+    unsafe {
+        *p2 += 1; // 将指针 p2指向的值加一
+    }
+    assert_eq!(values[1], 3); */
+
+    let b: u16 = 1500;
+    let b_: u8 = match b.try_into() {
+        Ok(result) => result,
+        Err(e) => {
+            println!("{:?}", e.to_string());
+            0
         }
     };
-    println!("{:?}", result); */
-    
-    /* 
-        into_iter, iter, iter_mut
-        在之前的代码中，我们统一使用了 into_iter 的方式将数组转化为迭代器，除此之外，还有 iter 和 iter_mut，聪明的读者应该大概能猜到这三者的区别：
+    println!("b_ is: {:?}", b_);
 
-        into_iter 会夺走所有权
-        iter 是借用
-        iter_mut 是可变借用
-        其实如果以后见多识广了，你会发现这种问题一眼就能看穿，into_ 之类的，都是拿走所有权，_mut 之类的都是可变借用，剩下的就是不可变借用。
-        详见下方示例：
-     */
-    // 1、 into_inter 所有权发生转移
-    let values = vec![1, 2, 3];
-    for v in values.into_iter() {
-        println!("{}", v);
-    }
-    // println!("{:?}", values); // 这里会报错，因为 values 在 .into_inter() 是发生了所有权转移
-    
-    // 2、 iter 不可变借用
-    let values2 = vec![1, 2, 3];
-    let values2_iter = values2.iter();
-    // if let Some(v) = values2_iter.next() { println!("{}", v); }  // 这里会报错，因为 values2_inter 是不可变借用，调用 next 方法时，会变更其内部属性值，所以会报错
-    println!("{:?}", values2);
-    
-    // 3、 iner_mut 可变借用
-    let mut values3 = vec![1, 2, 3];
-    let mut values3_iter_mut = values3.iter_mut();
-    if let Some(x) = values3_iter_mut.next() {
-        println!("{}", x); // 这里的x 是个可变引用
-        *x = 0; // 这里通过  * 解引用，取得 x 指向的值，并修改它的值
-    }
-    println!("{:?}", values3);
 
 }
